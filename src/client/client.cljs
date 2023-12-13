2fe4f5896ef8a898226a220293b351f7e75aa8d0
Ellis Kenyo <me@elken.dev>
2023-12-13 20:59:54 +0000

    fix: add react-refresh

--- src/client/client.cljs
+++ src/client/client.cljs
@@ -3,7 +3,7 @@
             [uix.dom :as dom.client]
             [app.ui :as ui]))
 
-(defn main []
+(defn init []
   (dom.client/hydrate-root
    (js/document.getElementById "root")
    ($ ui/title-bar)))
---
+++ src/client/preload.cljs
@@ -0,0 +1,10 @@
+(ns client.preload
+  (:require [uix.dev]))
+
+;; initializes fast-refresh runtime
+(uix.dev/init-fast-refresh!)
+
+;; called by shadow-cljs after every reload
+(defn ^:dev/after-load refresh []
+  ;; performs components refresh
+  (uix.dev/refresh!))
